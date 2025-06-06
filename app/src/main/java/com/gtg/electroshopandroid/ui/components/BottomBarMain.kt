2e84dc9e2c07da3952327ffd89de02dd1346c4eb
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-06-06 10:39:14 +0700

    modify navigation

--- app/src/main/java/com/gtg/electroshopandroid/ui/components/BottomBarMain.kt
+++ app/src/main/java/com/gtg/electroshopandroid/ui/components/BottomBarMain.kt
@@ -12,17 +12,25 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.gtg.electroshopandroid.data.model.Screen
 import com.gtg.electroshopandroid.data.model.bottomNavItems
+import com.gtg.electroshopandroid.preferences.TokenPreferences
 
 @Composable
-fun BottomBarMain(navController: NavHostController) {
+fun BottomBarMain(
+    navController: NavHostController,
+    tokenPreferences: TokenPreferences = TokenPreferences(LocalContext.current)
+) {
+    val accessToken by tokenPreferences.accessTokenFlow.collectAsState(initial = null)
     val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
     NavigationBar(
         containerColor = MaterialTheme.colorScheme.surface
@@ -60,7 +68,15 @@ fun BottomBarMain(navController: NavHostController) {
                 },
                 selected = currentRoute == screen.route,
                 onClick = {
-                    if (currentRoute != screen.route) {
+                    if (screen.route == Screen.Profile.route && accessToken == null) {
+                        navController.navigate(Screen.Login.route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    } else if (currentRoute != screen.route) {
                         navController.navigate(screen.route) {
                             // Pop up để tránh stack quá sâu
                             popUpTo(navController.graph.startDestinationId) {
