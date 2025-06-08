afcab279052121b08de4e41c21b3febbb8ffeba4
Chí Sơn <caosychison3011@gmail.com>
2025-06-09 01:14:36 +0700

    api filter

--- app/src/main/java/com/gtg/electroshopandroid/data/network/CategoryApiService.kt
+++ app/src/main/java/com/gtg/electroshopandroid/data/network/CategoryApiService.kt
@@ -5,15 +5,19 @@ import com.gtg.electroshopandroid.data.model.ProductDto
 import com.gtg.electroshopandroid.data.model.category.CategoryProductDto
 import retrofit2.http.GET;
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CategoryApiService {
 
-    @GET("api/Category/{id}")
-    suspend fun getCategory(
-        @Path("id") categoryId: Int
-    ): List<CategoryDto>
     @GET("api/Category/{id}/Product")
     suspend fun getProductbyCategoryId(
         @Path("id") categoryId: Int
+    ): List<CategoryProductDto>
+    @GET("api/Filter/category")
+    suspend fun filterProductsByCategory(
+        @Query("categoryId") categoryId: Int,
+        @Query("priceFilter") priceFilter: Int,
+        @Query("brandId") brandId: Int,
+        @Query("ratingFilter") ratingFilter: Int
     ): List<CategoryProductDto>
 }
